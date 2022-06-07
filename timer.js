af83f2d88cf94a2959bc5ac5869cefe9462eb939
Luke Damkoehler <ljdamkoehler@gmail.com>
2022-06-07 11:04:13 -0500

    First draft of JS timer app

---
+++ timer.js
@@ -0,0 +1,50 @@
+// Below is the Timer class code
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+        this.pauseButton.addEventListener('click', this.pause);
+        this.startButton.addEventListener('click', this.start);
+    };
+
+    start = () => {
+      if (this.onStart) {
+        this.onStart(this.timeRemaining);
+      }
+      this.tick();
+      this.intervalID = setInterval(this.tick, 20);
+    };
+
+    pause = () => {
+        clearInterval(this.intervalID);
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+              this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - .02;
+            if (this.onTick) {
+              this.onTick(this.timeRemaining);
+            }
+        }
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);      
+    };
+
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2);
+    };
+};
+// End Timer class code
